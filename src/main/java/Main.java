dc96344613127e630cc98f279512d8ebcc84935c
adko <adko@netcompany.com>
2020-07-20 11:51:27 +0200

    Some weird stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("World, I say hello, I do");
+        System.out.println("World, I say hello, I do do do");
     }
 }
