7b65fcd3a3f30f1d5685cd38a809b8166b07ca83
adko <adko@netcompany.com>
2020-07-28 10:51:41 +0200

    aaaaaaaaaaaaaaaaaaaaa

--- src/test/groovy/SomeClassSpec.groovy
+++ src/test/groovy/SomeClassSpec.groovy
@@ -28,7 +28,7 @@ class SomeClassSpec extends Specification {
         SomeClass.randomBehaviour(a, b) == 20
     }
 
-    def "a+b less than -2, or is it?uwou"() {
+    def "a+b less than -2, or is it?uwouo"() {
         given:
         def a = 11
         def b = -14
